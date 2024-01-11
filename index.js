c945683961487e87c3245ec48bd16763083db720
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-11 19:17:15 +0200

    feat: add api route with all data

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
 const http = require('http');
 // const url = require('url');
+
+const productsData = fs.readFileSync(`${ __dirname }/dev-data/data.json`);
+// const productsDataObj = JSON.parse(productsData);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
@@ -8,6 +12,9 @@ const server = http.createServer((req, res) => {
     res.end('You are on the overview page');
   } else if (pathName === '/product') {
     res.end('You are on the product page');
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(productsData);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
