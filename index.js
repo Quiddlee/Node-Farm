42e8f4caa1319ea73866b36f9db422656625e957
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-11 22:44:07 +0200

    feat: implement rendering the product cards list

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 const fs = require('fs');
 const http = require('http');
+
 // const url = require('url');
 
-const productsData = fs.readFileSync(`${ __dirname }/dev-data/data.json`);
-// const productsDataObj = JSON.parse(productsData);
+function replaceTemplate(temp, product) {
+  let output = temp
+    .replaceAll('{%PRODUCTNAME%}', product.productName)
+    .replaceAll('{%IMAGE%}', product.image)
+    .replaceAll('{%PRICE%}', product.price)
+    .replaceAll('{%FROM%}', product.from)
+    .replaceAll('{%NUTRIENTS%}', product.nutrients)
+    .replaceAll('{%QUANTITY%}', product.quantity)
+    .replaceAll('{%DESCRIPTION%}', product.description)
+    .replaceAll('{%ID%}', product.id);
+
+  if (!product.organic) output = output.replaceAll('{%NOT_ORGANIC%}', 'not-organic');
+
+  return output;
+}
+
+const data = fs.readFileSync(`${ __dirname }/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const tempOverview = fs.readFileSync(`${ __dirname }/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${ __dirname }/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${ __dirname }/templates/template-card.html`, 'utf-8');
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // Overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('You are on the overview page');
+    res.writeHead(200, { 'Content-type': 'text/html', });
+
+    const cardsHtml = dataObj.map(product => replaceTemplate(tempCard, product)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
+
+    // Product page
   } else if (pathName === '/product') {
     res.end('You are on the product page');
+
+    // API
   } else if (pathName === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
-    res.end(productsData);
+    res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
