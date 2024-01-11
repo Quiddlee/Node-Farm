8e7e3bd5398d6d2710db16759e6b8cd99b14676e
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-11 18:44:05 +0200

    feat: initialize server & routing

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+// const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('You are on the overview page');
+  } else if (pathName === '/product') {
+    res.end('You are on the product page');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
